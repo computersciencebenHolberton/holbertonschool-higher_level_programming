40efa39a14ac074e1dbcb478249b2f663f630960
computersciencebenHolberton <computersciencebenHolberton@gmail.com>
2019-09-13 01:18:27 +0000

    python

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-from variable_load_5 import a #import a from Variable_
+from variable_load_5 import a  # import a from Variable_
 if __name__ == "__main__":
     print("{}".format(a))
