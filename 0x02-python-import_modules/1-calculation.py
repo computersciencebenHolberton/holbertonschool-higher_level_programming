4c93057ff1497785c13806c672ca95120678ad56
computersciencebenHolberton <computersciencebenHolberton@gmail.com>
2019-09-13 01:04:40 +0000

    python

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div #using math im didnt work?
+from calculator_1 import add, sub, mul, div  # using math im didnt work?
 if __name__ == "__main__":
-    a = 10 # value 10 to a
-    b = 5 #value 5 to a 
+    a = 10  # value 10 to a
+    b = 5  # value 5 to a
     print("{} + {} = {}".format(a, b, add(a, b)))
     print("{} - {} = {}".format(a, b, sub(a, b)))
     print("{} * {} = {}".format(a, b, mul(a, b)))
